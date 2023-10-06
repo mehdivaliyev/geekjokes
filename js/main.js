f0c9881763a9c044e6ac3bcf6961838439c3d905
mehdivaliyev <valiyevmehdi@gmail.com>
2023-10-06 20:10:02 +0400

    master

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function fetchData() {
+  $.ajax({
+    url: "https://geek-jokes.sameerkumar.website/api?format=json",
+    method: "GET",
+    success: (data) => {
+      $("#joke").text(data.joke);
+    }
+  });
+}
+
+$("#btn").click(fetchData);
